a43a51899782db61bb20161a9095f8d778478df7
Nate <nate.d.gage@gmail.com>
2022-10-07 14:59:39 -0600

    Added mapbox styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
 mapboxgl.accessToken =
-  "pk.eyJ1IjoibmF0ZWdhZ2UiLCJhIjoiY2w4eXRoeGVjMGlyMzNvbnpoY25pbjQ5MyJ9.REs9iG9rPkhfZ3ZdS-t8Kw";
+  "pk.eyJ1IjoibmF0ZWdhZ2UiLCJhIjoiY2p5dGc4ZTFyMDN1NzNibWhzdHBjY21ocyJ9.nH4wTRexSr3sL_ri-JGHOQ";
 
 function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng] = useState(-70.9);
-  const [lat] = useState(42.35);
-  const [zoom] = useState(9);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -21,11 +21,19 @@ function App() {
     });
   });
 
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <p>MAPBOX DEMO</p>
-        <p>The Map:</p>
         <div ref={mapContainer} className="map-container" />
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import "mapbox-gl/dist/mapbox-gl.css";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <script src="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.js"></script>
-    <link
-      href="https://api.mapbox.com/mapbox-gl-js/v2.10.0/mapbox-gl.css"
-      rel="stylesheet"
-    />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
