81881ac1837c56803cf82c66e5d49d87e404183f
Nate <nate.d.gage@gmail.com>
2022-10-09 17:32:58 -0600

    Updated handleSubmit func

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -1,22 +1,23 @@
-const RESOURCE_CREATED = 201
+const RESOURCE_CREATED = 201;
 
 //Data would normally be managed by a back-end database
 //In this case I simply hold the data in memory
-let temperatureData
+let temperatureData;
 
 // I made these functions asynchronous since these would normally
 // be doing, for example, network calls to a database
 
 const getTemps = (req, res) => {
-    res.send(temperatureData);
-}
+  res.send(temperatureData);
+};
 
 const saveTemps = (req, res) => {
-    temperatureData = req.body
-    res.status(RESOURCE_CREATED).send();
-}
+  temperatureData = req.body;
+  console.log("TEMP DATA: " + temperatureData);
+  res.status(RESOURCE_CREATED).send();
+};
 
 module.exports = {
-    getTemps,
-    saveTemps
-}
+  getTemps,
+  saveTemps,
+};
--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React, { useRef, useEffect, useState } from "react";
 import axios from "axios";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import Grid from "@mui/material/Grid";
 import LinearProgress from "@mui/material/LinearProgress";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
 
 mapboxgl.accessToken = process.env.REACT_APP_ACCESS_TOKEN;
 
 function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const [file, setFile] = useState();
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(false);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [locations, setLocations] = useState([]);
+  const [lng, setLng] = useState(-104.995);
+  const [lat, setLat] = useState(39.765);
+  const [zoom, setZoom] = useState(3);
 
-  const mystuff = {
-    some: "stuff",
-    to: "add",
-  };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     // await axios.post("http://localhost:3300/");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios.post("http://localhost:3300/", mystuff);
+  //     await axios.get("http://localhost:3300/").then((res) => {
+  //       setLocations(res.data);
+  //     });
+  //   };
+  //   fetchData();
+  // }, []);
 
-      await axios.get("http://localhost:3300/").then((res) => {
-        setData(JSON.stringify(res.data));
-      });
-    };
-    fetchData();
-  }, []);
-
+  //Initialize map
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -52,8 +52,41 @@ function App() {
     });
   });
 
-  const handleChangeFile = (e) => {
-    console.log("file changed: " + e.target.value);
+  // add markers to map
+  useEffect(() => {
+    for (const city of locations) {
+      // create a HTML element for each feature
+      const el = document.createElement("div");
+      el.className = "marker";
+
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el).setLngLat(city.coordinates).addTo(map.current);
+    }
+  }, [locations]);
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = "http://localhost:3300/";
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", file.name);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
+
+    console.log(formData);
+
+    // await axios.post(url, formData, config);
+
+    // await axios.get("http://localhost:3300/").then((res) => {
+    //   setLocations(res.data);
+    // });
   };
 
   return (
@@ -63,9 +96,13 @@ function App() {
           <img
             src="../mapbox_logo.png"
             style={{ height: "40%", marginBottom: "30px" }}
+            alt="logo"
           />
-          <h4 style={{ marginBottom: "0px" }}>UPLOAD A FILE</h4>
-          <input type="file" onChange={(e) => handleChangeFile(e)} />
+          <h6 style={{ marginBottom: "0px" }}>Upload a File</h6>
+          <form onSubmit={handleSubmit}>
+            <input type="file" id="input" onChange={handleChange} />
+            <button type="submit">Upload</button>
+          </form>
           {loading && (
             <LinearProgress
               style={{
@@ -74,14 +111,20 @@ function App() {
               }}
             />
           )}
-          <Button
-            variant="contained"
-            size="large"
-            sx={{ width: "80%", marginTop: "50px" }}
-            disabled={data}
-          >
-            Convert to Celsius
-          </Button>
+          <FormControl sx={{ marginTop: "30px" }}>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+              <FormControlLabel
+                value="a"
+                control={<Radio />}
+                label="Farenheit"
+              />
+              <FormControlLabel value="b" control={<Radio />} label="Celsius" />
+            </RadioGroup>
+          </FormControl>
         </Grid>
         <Grid item xs={9}>
           <div
@@ -90,7 +133,6 @@ function App() {
             style={{ border: "2px solid #1976d2" }}
           />
         </Grid>
-        <p>{data}</p>
       </Grid>
     </Box>
   );
